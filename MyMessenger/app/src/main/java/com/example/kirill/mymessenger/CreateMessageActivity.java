320ae8a599dabbbb84307f4097086e665a6f6082
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-10-24 23:10:28 +0300

    Добавил неявный интент для отправки сообщений
    Теперь можно выбрать приложение, через которое будет отправленно сообщение

--- MyMessenger/app/src/main/java/com/example/kirill/mymessenger/CreateMessageActivity.java
+++ MyMessenger/app/src/main/java/com/example/kirill/mymessenger/CreateMessageActivity.java
@@ -19,13 +19,12 @@ public class CreateMessageActivity extends Activity {
         EditText messageView = (EditText) findViewById(R.id.message);
         String messageText = messageView.getText().toString();
 
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+//        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+//        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
 
-        // NEW LINE
-//        Intent intent = new Intent(Intent.ACTION_SEND);
-//        intent.setType("text/plain");
-//        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
 
         startActivity(intent);
     }
